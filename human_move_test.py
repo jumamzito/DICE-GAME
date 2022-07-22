d5b42b80c37ae949430ffe1c507657c058d93ffd
jumamzito <zjwafula@gmail.com>
2022-07-22 15:50:23 +0300

    Add files via upload
    Minor changes
    work on the game over function

--- human_move_test.py
+++ human_move_test.py
@@ -12,7 +12,7 @@ def human_move(human_score,computer_score) :
         if roll_request == "y" or roll_request == "Y":
             roll = random.randint(1,6)
             if roll == 6:
-                human_score +=0
+                human_score =0
                 print(f"The roll gave you {roll}") 
                 print(f'Your current score is {human_score}')
                 break
@@ -27,17 +27,25 @@ def human_move(human_score,computer_score) :
     return human_score,computer_score
 
 def computer_move(human_score,computer_score):
-    roll = random.randint(1,6)
-    if roll == 6:
-        computer_score +=0
-        print(f"The roll gave you {roll}") 
-        print(f'Your current score is {computer_score}')
-    else:
-        human_score =  human_score + roll 
-        print(f'roll gave you: {roll}')
-        print(f'current score is: {human_score}')
+    
+    while computer_score !=30:
+        roll = random.randint(1,6)
+
+        if roll == 6:
+            computer_score =0
+            print(f"The roll gave you {roll}") 
+            print(f'Your current score is {computer_score}')
+            break
+        else:
+            computer_score =  computer_score + roll 
+            print(f'roll gave you: {roll}')
+            print(f'current score is: {computer_score}')
 
     return human_score,computer_score
 
 human_move(human_score,computer_score)
+computer_move(human_score,computer_score)
+def is_game_over(computer_score, human_score):
+    if computer_score or human_score ==50:
+        print('Game is over: ')
 #print(human_score)
